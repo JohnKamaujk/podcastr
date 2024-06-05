356ffd35d23da95ffe88031d6276dfc0d4555f84
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-05 20:35:36 +0300

    :sparkles:chore:root page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-white-1">
+      <p>Trending podcasts</p>
+    </div>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <div className="mt-9">Home</div>;
-};
-
-export default Home;
