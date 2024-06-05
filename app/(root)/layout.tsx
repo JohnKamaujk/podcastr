6cf52053ca0835251a5e6358b1b767edb74f3a33
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-05 22:08:57 +0300

    :construction:feat:root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import LeftSidebar from "@/components/LeftSidebar";
 
 export default function RootLayout({
@@ -9,7 +10,20 @@ export default function RootLayout({
     <div className="relative flex flex-col">
       <main className="relative flex bg-black-3">
         <LeftSidebar />
-        <div>{children}</div>
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              <div>mobile nav</div>
+            </div>
+            <div className="flex flex-col md:pb-14">{children}</div>
+          </div>
+        </section>
         <p className="text-white-1">RIGHT SIDEBAR</p>
       </main>
     </div>
