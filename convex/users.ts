1909f2bd673cb59c9604de265e5556b1c3b33c6c
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-07 13:21:19 +0300

    :sparkles:feat:update user action

--- convex/users.ts
+++ convex/users.ts
@@ -18,3 +18,40 @@ export const createUser = internalMutation({
     });
   },
 });
+
+export const updateUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    imageUrl: v.string(),
+    email: v.string(),
+  },
+  async handler(ctx, args) {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.patch(user._id, {
+      imageUrl: args.imageUrl,
+      email: args.email,
+    });
+
+    const podcast = await ctx.db
+      .query("podcasts")
+      .filter((q) => q.eq(q.field("authorId"), args.clerkId))
+      .collect();
+
+    await Promise.all(
+      podcast.map(async (p) => {
+        await ctx.db.patch(p._id, {
+          authorImageUrl: args.imageUrl,
+        });
+      })
+    );
+  },
+});
+
