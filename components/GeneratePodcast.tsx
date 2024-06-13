a4ecce4e6bc3b228fefcc7065456f41b4bf46c8c
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-13 13:35:50 +0300

    :sparkles:feat:generate podcast auddio url

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -4,9 +4,11 @@ import { Textarea } from "./ui/textarea";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Loader } from "lucide-react";
-import { useAction } from "convex/react";
+import { useAction, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import { v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
+
+import { useUploadFiles } from "@xixixao/uploadstuff/react";
 
 const useGeneratePodcast = ({
   setAudio,
@@ -15,6 +17,9 @@ const useGeneratePodcast = ({
   setAudioStorageId,
 }: GeneratePodcastProps) => {
   const [isGenerating, setIsGenerating] = useState(false);
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const { startUpload } = useUploadFiles(generateUploadUrl);
 
   const getPodcastAudio = useAction(api.openai.generateAudioAction);
 
@@ -30,10 +35,15 @@ const useGeneratePodcast = ({
         voice: voiceType,
         input: voicePrompt,
       });
+
       const blob = new Blob([response], { type: "audio/mpeg" });
       const fileName = `podcast-${uuidv4()}.mp3`;
       const file = new File([blob], fileName, { type: "audio/mpeg" });
 
+      const uploaded = await startUpload([file]);
+      const storageId = (uploaded[0].response as any).storageId;
+
+      setAudioStorageId(storageId);
     } catch (error) {
       console.log("Error generating podcast", error);
       setIsGenerating(false);
