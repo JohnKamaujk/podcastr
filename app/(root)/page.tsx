8465d125e90f16fd632da8513db163b90492c008
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-19 11:54:40 +0300

    :sparkles:get trendy podcast

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,13 +13,6 @@ const Home = () => {
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <div className="flex min-h-screen flex-col items-center justify-between p-24 ">
-          {/* {tasks?.map(({ _id, text }) => (
-            <p className="text-white-1" key={_id}>
-              {text}
-            </p>
-          ))} */}
-        </div>
         <div className="podcast_grid">
           {podcastData.map(({ id, title, description, imgURL }) => (
             <PodcastCard
