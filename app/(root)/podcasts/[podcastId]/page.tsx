f3596a2091c60eddf36906848e5ae2f92b495a71
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-29 13:05:56 +0300

    :sparkles:feat:podacast details page

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
+import PodcastDetailPlayer from "@/components/PodcastDetailPlayer";
 
 const PodcastDetails = ({
   params: { podcastId },
@@ -25,6 +26,27 @@ const PodcastDetails = ({
           <h2 className="text-16 font-bold text-white-1">{podcast?.views}</h2>
         </figure>
       </header>
+
+      <PodcastDetailPlayer />
+
+      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">
+        {podcast?.podcastDescription}
+      </p>
+
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-18 font-bold text-white-1">Transcription</h1>
+          <p className="text-16 font-medium text-white-2">
+            {podcast?.voicePrompt}
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-18 font-bold text-white-1">Thumbnail Prompt</h1>
+          <p className="text-16 font-medium text-white-2">
+            {podcast?.imagePrompt}
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
