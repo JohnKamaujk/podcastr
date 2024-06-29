7e579b6cd176d3e826c2455423c6f3e83f212aa7
JohnKamaujk <johnjkkamau@gmail.com>
2024-06-29 13:31:08 +0300

    :sparkles:feat:delete podcast

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "proper-parakeet-118.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
