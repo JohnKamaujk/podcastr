3794316f05e196485fbf8407e0c0e586987b510e
JohnKamaujk <johnjkkamau@gmail.com>
2024-07-03 13:00:14 +0300

    :sparkles:feat:User right sidebar profile

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,10 +1,31 @@
+"use client";
+
+import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const RightSidebar = () => {
+  const { user } = useUser();
   return (
-    <div>
-      <p className="text-white-1">Right sidebar</p>
-    </div>
+    <section className="right_sidebar h-[calc(100vh-5px)]">
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
+          <UserButton />
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-16 truncate font-semibold text-white-1">
+              {user?.firstName} {user?.lastName}
+            </h1>
+            <Image
+              src="/icons/right-arrow.svg"
+              alt="arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </Link>
+      </SignedIn>
+    </section>
   );
 };
 
